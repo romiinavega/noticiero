b560fd998db2ffe5b4943aec51861a5fd36060c1
Araceli Arizmendi <araceli.muse@gmail.com>
2022-01-31 18:48:22 -0700

    BD

--- Noticiero.sql
+++ Noticiero.sql
@@ -1,24 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 31, 2022 at 10:39 PM
--- Server version: 5.7.34
--- PHP Version: 7.4.21
+-- Generation Time: Feb 01, 2022 at 01:44 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `Noticiero`
+-- Database: `noticiero`
 --
 
 -- --------------------------------------------------------
@@ -42,7 +35,11 @@ CREATE TABLE `noticias` (
 --
 
 INSERT INTO `noticias` (`id`, `titulo`, `autor`, `fecha`, `noticia`, `created_at`, `updated_at`) VALUES
-(1, 'Gato se cayó', 'Carlos Encinas', '2022-01-24', 'noooooooo se cayó un gatooooo!!!\r\n\r\nMIUAAAAUUUU', '2022-01-13 01:41:31', '2022-01-14 01:41:31');
+(1, 'Gato se cayó', 'kaka Encinas', '2022-01-24', 'noooooooo se cayó un gatooooo!!!\r\n\r\nMIUAAAAUUUU', '2022-02-01 01:16:58', '2022-02-01 08:16:58'),
+(2, 'se levanto el gato', 'yo', '2022-01-31', 'una vez se levanto el michi', '2022-02-01 01:09:57', '2022-02-01 08:09:57'),
+(3, 'Gato se cayó', 'Carlos Encinas', '2022-01-24', 'se cayó un gatooooo!!!\r\n\r\nMIUAAAAUUUU', '2022-02-01 07:32:47', '2022-02-01 07:32:47'),
+(4, 'se ldurmio el gato', 'rominola', '2022-01-31', 'una vez se levanto el michi', '2022-02-01 07:32:59', '2022-02-01 07:32:59'),
+(5, 'aaa', 'rominola', '2022-01-31', 'una vez se levanto el michi', '2022-02-01 07:33:21', '2022-02-01 07:33:21');
 
 --
 -- Indexes for dumped tables
@@ -62,9 +59,4 @@ ALTER TABLE `noticias`
 -- AUTO_INCREMENT for table `noticias`
 --
 ALTER TABLE `noticias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
